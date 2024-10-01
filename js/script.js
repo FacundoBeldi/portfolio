158d318beebd9f348c410a0417f5584a20bf903a
FacundoBeldi <facu.beldi@hotmail.com>
2024-10-01 12:09:56 -0300

    changes, still need to do the sweetalert alert

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ closeBtn.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+//EmailJS
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del formulario
+
+    // Enviar el formulario usando EmailJS
+    emailjs.sendForm('service_t261z8s', 'template_4qzw68c', this)
+        .then(function () {
+            Swal.fire({
+                icon: "Good job!",
+                title: "Genial!",
+                text: "Mensaje enviado con exito!",
+              });
+            document.getElementById('contactForm').reset(); // Opcional: Reinicia el formulario
+        }, function (error) {
+            alert('Error al enviar el mensaje: ' + JSON.stringify(error));
+        });
+});
