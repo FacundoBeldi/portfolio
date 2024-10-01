d305e1f7dc962298f957f0f5d87c281241b66279
FacundoBeldi <facu.beldi@hotmail.com>
2024-10-01 20:27:38 -0300

    finished

--- js/script.js
+++ js/script.js
@@ -25,12 +25,36 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     emailjs.sendForm('service_t261z8s', 'template_4qzw68c', this)
         .then(function () {
             Swal.fire({
-                icon: "Good job!",
+                icon: "success", // Icono de éxito
                 title: "Genial!",
-                text: "Mensaje enviado con exito!",
-              });
-            document.getElementById('contactForm').reset(); // Opcional: Reinicia el formulario
+                text: "Mensaje enviado con éxito!",
+            });
+            document.getElementById('contactForm').reset(); // Reinicia el formulario
         }, function (error) {
-            alert('Error al enviar el mensaje: ' + JSON.stringify(error));
+            Swal.fire({
+                icon: "error", // Icono de error
+                title: "Oops...",
+                text: "Hubo un error al enviar el mensaje. Por favor, intenta de nuevo.",
+                footer: '<a href="mailto:facu.beldi@gmail.com">Contactame si el problema persiste</a>'
+            });
         });
+});
+
+//Observer
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate__animated', 'animate__fadeInUp'); // Agrega clases de Animate.css
+            observer.unobserve(entry.target); // Deja de observar el elemento una vez animado (opcional)
+        }
+    });
+});
+
+// Selecciona todos los elementos que tienen la clase 'animate-on-scroll'
+const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
+
+// Aplica el observer a cada uno de los elementos
+elementsToAnimate.forEach(element => {
+    observer.observe(element);
 });
